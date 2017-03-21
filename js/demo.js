17e264ee331f1785663db9c41fd7127e1d67743c
oldchicken <lrw0401@sina.com>
2017-03-21 10:57:41 +0800

    引入AJAX，并使用表单序列化插件

--- js/demo.js
+++ js/demo.js
@@ -384,8 +384,18 @@ $(function(){
             flag=false;
         }
         
-        if(flag)$('form').first().submit();
-    })
+        if (flag) {
+			ajax({
+				method : 'post',
+				url : 'demo.php',
+				data : $('form').eq(0).serialize(),
+				success : function (text) {
+					alert(text);
+				},
+				async : true
+			});
+		}
+    });
     
     
     //登陆框
@@ -756,47 +766,3 @@ $(function(){
     
 });
 
-
-
-
-
-/*
-window.onload = function () {
-	//个人中心的下拉菜单
-	$().getClass('member').hover(function () {
-		//$().getClass('member').css('background', 'url(images/arrow2.png) no-repeat 55px center');
-		//this.css('color', 'red');
-		$(this).css('background', 'url(images/arrow2.png) no-repeat 55px center');
-		$().getClass('member_ul').show();
-	}, function () {
-		//$().getClass('member').css('background', 'url(images/arrow.png) no-repeat 55px center');
-		$(this).css('background', 'url(images/arrow.png) no-repeat 55px center');
-		$().getClass('member_ul').hide();
-	});
-     
-    var login=$().getId('login');
-    var screen=$().getId('screen');
-    login.center(250,350);
-    $().resize(function(){
-        login.center(250,350);
-        if(login.css('display')=='block'){
-            screen.lock();
-            };
-    });
-    $().getClass('login').click(function(){
-        login.center(250,350);
-        login.css('display','block');
-        screen.lock();
-    })
-    $().getClass('close').click(function(){
-        login.css('display','none');
-        screen.unlock();
-    });
-    
-    
-    
-    
-    
-    login.drag([$().getTagName('h2').getElement(0),$().getTagName('span').getElement(0)]);
-    
-};*/
