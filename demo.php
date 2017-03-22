cbd371035fceae8a6853166eb1a0467a042b716c
oldchicken <lrw0401@sina.com>
2017-03-22 09:56:53 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 <?php
-	require 'config.php';
-    
-    $query = "INSERT INTO blog_user (user, pass, ques, ans, email, birthday, ps) 
-												VALUES ('{$_POST['user']}', sha1('{$_POST['pass']}'), '{$_POST['ques']}', '{$_POST['ans']}', '{$_POST['email']}', '{$_POST['birthday']}', '{$_POST['ps']}')";
-      
-    mysql_query($query) or die('新增失败！'.mysql_error());
-      
-    sleep(3);
-
-    echo mysql_affected_rows();
-
-    mysql_close();
-?>
-=======
-﻿<?php
 	//echo 'www.ycku.com';
 	print_r($_POST);
 ?>
->>>>>>> origin/master
